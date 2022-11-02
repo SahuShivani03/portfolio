8bf823a85fdd04bf550b7c8ec194ffa29782ced6
Shivani sahu <102249991+SahuShivani03@users.noreply.github.com>
2022-11-02 11:26:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function geeksforgeeks() {
+    var x = document.getElementById("menus");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
